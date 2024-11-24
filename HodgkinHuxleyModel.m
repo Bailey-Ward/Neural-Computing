abc48b02905fa7288eaa20109137d4e183aca013
Bailey <72494693+Bailey-Ward@users.noreply.github.com>
2024-11-24 15:44:57 +0000

    Add files via upload

--- HodgkinHuxleyModel.m
+++ HodgkinHuxleyModel.m
@@ -33,20 +33,21 @@ V_values = zeros(size(time));
 
 % Define rate functions for gating variables
 
+alpha_m =@(V) (10^5 * (-Vm - 0.045)) / (exp(100 * (-Vm- 0.045)) - 1);
+beta_m =@(V) 4 * 10^3 * exp((-Vm - 0.070) / 0.018);
+alpha_h =@(V) 70 * exp(50 * (-Vm - 0.070));
+beta_h =@(V) 10^3 / (1 + exp(100 * (-Vm - 0.040)));
+alpha_n =@(V) (10^4 * (-Vm - 0.060)) / (exp(100 * (-Vm - 0.060)) - 1);
+beta_n =@(V) 125 * exp((-Vm - 0.070) / 0.08);
+
 
 % Simulation loop
 for t = 2:length(time)
-    alpha_m =@(V) (10^5 * (-Vm - 0.045)) / (exp(100 * (-Vm- 0.045)) - 1);
-    beta_m =@(V) 4 * 10^3 * exp((-Vm - 0.070) / 0.018);
-    alpha_h =@(V) 70 * exp(50 * (-Vm - 0.070));
-    beta_h =@(V) 10^3 / (1 + exp(100 * (-Vm - 0.040)));
-    alpha_n =@(V) (10^4 * (-Vm - 0.060)) / (exp(100 * (-Vm - 0.060)) - 1);
-    beta_n =@(V) 125 * exp((-Vm - 0.070) / 0.08);
 
     % Update gating variables using Euler method
-    m = m + dt * (alpha_m(V(t-1)) * (1 - m) - beta_m(V(t-1)) * m);
-    h = h + dt * (alpha_h(V(t-1)) * (1 - h) - beta_h(V(t-1)) * h);
-    n = n + dt * (alpha_n(V(t-1)) * (1 - n) - beta_n(V(t-1)) * n);
+    m = m + dt * (alpha_m(Vm) * (1 - m) - beta_m(Vm) * m);
+    h = h + dt * (alpha_h(Vm) * (1 - h) - beta_h(Vm) * h);
+    n = n + dt * (alpha_n(Vm) * (1 - n) - beta_n(Vm) * n);
     
     %dm = (alpha_m * (1-m)) - (beta_m * m);
     %dh = (alpha_h * (1-h)) - (beta_h * h);
@@ -62,13 +63,14 @@ for t = 2:length(time)
     I_L = g_L * (V(t-1) - E_L);
     
     % Update membrane potential using Euler's method
-    V(t) = V(t-1) + dt * (Iapp(t) - (I_Na + I_K + I_L)) / C_m;
-    
+    dVm = (I_Na + I_K + I_L + Iapp(time(t))) / Cm;
+    Vm = Vm + dt * dVm;
+
     % Store values for plotting
-    m_values(t) = m;
-    h_values(t) = h;
-    n_values(t) = n;
-    V_values(t) = V(t);
+    m_values(t+1) = m;
+    h_values(t+1) = h;
+    n_values(t+1) = n;
+    V_values(t+1) = Vm;
 end
 
 % Plot membrane potential over time
